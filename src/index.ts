6da770095f2987e080c6510513025dedcd010ced
Krenar Drala <krenar.drala@gecko.mk>
2019-08-29 14:05:04 +0200

    Fixed design issues and removed unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/_custom.scss';
 import ListRecipes from './ListRecipes';
 
-let gameElem = document.getElementById("app") as HTMLElement;
-
 let reciepe = new ListRecipes();
 
 
